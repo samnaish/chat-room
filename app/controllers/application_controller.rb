aac4046c2c3f5fd53ce28f0b970e276b56d8081d
Sam Naish <sam.naish92@googlemail.com>
2020-10-25 20:29:22 +0000

    added chat-box and styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+
+    def current_user
+        return @curent_user if @curent_user.present?
+        if session[:user_id].present?
+            @curent_user = User.find(session[:user_id])
+        else
+            @curent_user = User.generate
+            session[:user_id] = @curent_user.id
+            @curent_user
+        end
+    end
+    
+
 end
